5b0772b0f64b9b0a182c4e076cf7d6321feb2aa9
Nikolai Cinotti <eaenikolai@gmail.com>
2025-07-20 13:55:26 -0300

    update live header icon

--- src/resources/icons.ts
+++ src/resources/icons.ts
@@ -41,11 +41,11 @@ import {
   FaFacebook,
 } from "react-icons/fa6";
 
-import { RiLiveFill } from "react-icons/ri";
+import { RiLiveLine } from "react-icons/ri";
 
 export const iconLibrary: Record<string, IconType> = {
   arrowUpRight: HiArrowUpRight,
-  live: RiLiveFill,
+  live: RiLiveLine,
   facebook: FaFacebook,
   tiktok: FaTiktok,
   spotify: FaSpotify,
