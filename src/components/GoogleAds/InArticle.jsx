1e0bcbea28667b806480f64ab95c6a9fbf684bc7
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-08 20:43:57 -0300

    add google ads folder and try manual add

---
+++ src/components/GoogleAds/InArticle.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+export default function GoogleAd() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("Erro ao carregar Google Ads", e);
+    }
+  }, []);
+
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7328591460493456"
+        crossOrigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-7328591460493456"
+        data-ad-slot="3014596360"
+      ></ins>
+    </>
+  );
+}
