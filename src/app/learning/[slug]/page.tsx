e3270f21e3886e44b8807cfc403bc034be9b0c30
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-08 22:26:10 -0300

    Update page.tsx

--- src/app/learning/[slug]/page.tsx
+++ src/app/learning/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { baseURL, about, person, learning } from "@/resources";
 import { formatDate } from "@/utils/formatDate";
 import { ScrollToHash, CustomMDX } from "@/components";
 import { Metadata } from "next";
-import InArticleAd from '@/components/googleAds/InArticleAd';
+// import InArticleAd from '@/components/googleAds/InArticleAd';
 
 
 
@@ -95,7 +95,19 @@ export default async function Project({
           </Text>
         </Flex>
         <CustomMDX source={post.content} />
-        <InArticleAd />
+        <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7328591460493456"
+        crossOrigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-7328591460493456"
+        data-ad-slot="3014596360"
+      ></ins>
       </Column>
       <ScrollToHash />
     </Column>
