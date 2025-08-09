609da26e40a9806ac86fc562366aab247bed7d65
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-09 09:49:11 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
       padding="8"
       horizontal="center"
       mobileDirection="column"
-      
+      minHeight="80"
     >
       <Flex
         className={styles.mobile}
