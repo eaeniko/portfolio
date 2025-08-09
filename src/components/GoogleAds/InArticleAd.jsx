3adf1845804027f453bc4e8ba4eb8f1ed6bf7ad9
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-08 21:44:06 -0300

    update google ads snippet component

--- src/components/GoogleAds/InArticleAd.jsx
+++ src/components/GoogleAds/InArticleAd.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export default function GoogleAd() {
+export default function InArticleAd() {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
