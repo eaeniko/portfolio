ac177a2e92f887e9f986d96a62b6eadad57fd730
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-08 23:08:51 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -159,8 +159,8 @@ export default async function RootLayout({
                 </RouteGuard>
               </Flex>
             </Flex>
-            {/* Adiciona um espaço reservado para anúncios automáticos acima do footer */}
-            <Column gap="xl" style={{ marginBottom: '40px', minHeight: '340px' }} />
+            {/* Adiciona um espaço reservado para anúncios automáticos acima do footer 
+             <Column gap="xl" style={{ marginBottom: '40px', minHeight: '340px' }} />*/}
             <Footer/>
           </Column>
           <SpeedInsights />
