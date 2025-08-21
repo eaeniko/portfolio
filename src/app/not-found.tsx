9e36775c020f4d6ca11e9d3092afecd10cf04647
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-21 10:55:17 -0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,9 @@
 import { Column, Heading, Text } from "@once-ui-system/core";
+import { LayoutProvider } from "@once-ui-system/core"; // or correct path if different
 
 export default function NotFound() {
   return (
+    <LayoutProvider>
     <Column as="section" fill center paddingBottom="160">
       <Text marginBottom="s" variant="display-strong-xl">
         404
@@ -11,5 +13,6 @@ export default function NotFound() {
       </Heading>
       <Text onBackground="neutral-weak">The page you are looking for does not exist.</Text>
     </Column>
+    </LayoutProvider>
   );
 }
