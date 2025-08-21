13ced667fa21e5493658c0527620532fd818c286
Nikolai Cinotti <eaenikolai@gmail.com>
2025-08-21 12:03:31 -0300

    Update page.tsx

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -13,7 +13,7 @@ import {
   OgCard,
   SmartLink,
 } from "@once-ui-system/core";
-import { content, social } from "@/resources";
+import { bio, socials } from "@/resources";
 
 interface LinkItem {
   title?: string;
@@ -29,29 +29,29 @@ export default function Home() {
   return (
     <Column fillWidth horizontal="center" padding="16">
       <Column maxWidth="m" aspectRatio="2/1" marginBottom="40" paddingY="24">
-        {content.cover && (
+        {bio.cover && (
           <Row fill position="absolute" left="0" top="0" radius="xl" overflow="hidden" border="neutral-alpha-weak">
-            <Media sizes="(max-width: 768px) 100vw, 960px" priority fill src={content.cover} alt={content.name}/>
+            <Media sizes="(max-width: 768px) 100vw, 960px" priority fill src={bio.cover} alt={bio.name}/>
             <Fade fill position="absolute" to="top" bottom="0" left="0" pattern={{ display: true, size: "2" }}/>
           </Row>
         )}
         <Column fill center padding="l" gap="4" align="center">
-          {content.avatar && (
-            <Avatar src={content.avatar} size="xl" />
+          {bio.avatar && (
+            <Avatar src={bio.avatar} size="xl" />
           )}
-          {content.name && (
-            <Heading variant="heading-strong-xl" marginTop={content.avatar ? "24" : undefined}>
-              {content.name}
+          {bio.name && (
+            <Heading variant="heading-strong-xl" marginTop={bio.avatar ? "24" : undefined}>
+              {bio.name}
             </Heading>
           )}
-          {content.bio && (
+          {bio.bio && (
             <Text variant="body-default-l" onBackground="neutral-weak">
-              {content.bio}
+              {bio.bio}
             </Text>
           )}
-          {social?.length > 0 && (
+          {socials?.length > 0 && (
             <Row gap="16" paddingX="l" paddingTop="24" horizontal="center" fillWidth wrap>
-              {social.map((item: any, index: any) => (
+              {socials.map((item: any, index: any) => (
                 <IconButton
                   key={index}
                   href={item.link}
@@ -68,9 +68,9 @@ export default function Home() {
       </Column>
       
       <Column maxWidth="s" gap="24">
-        {content.links?.length > 0 && (
+        {bio.links?.length > 0 && (
           <>
-            {(content.links as LinkItem[]).map((link, index) => (
+            {(bio.links as LinkItem[]).map((link, index) => (
               <OgCard
                 background="transparent"
                 border="transparent"
@@ -92,9 +92,9 @@ export default function Home() {
           <Text onBackground="neutral-weak">
             {new Date().getFullYear() + " "}
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Bio teste{" "}
+            / Build your bio with{" "}
             <SmartLink
-              href="https://eaeniko.vercel.app"
+              href="https://once-ui.com/products/magic-bio"
             >
               Once UI
             </SmartLink>
