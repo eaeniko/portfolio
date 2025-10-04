d77a5d28d4acfa3bc79f55534f73090ab3b1b4bb
Nikolai Cinotti <eaenikolai@gmail.com>
2025-10-03 23:29:53 -0300

    Update page.tsx

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -30,7 +30,7 @@ export default function Live() {
 
         </div>
 
-        <iframe src="https://discord.com/widget?id=397707232512311296&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        <iframe src="https://discord.com/widget?id=397707232512311296&theme=dark" width="350" height="500" allowTransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
       </Column>
       
     </Column>
