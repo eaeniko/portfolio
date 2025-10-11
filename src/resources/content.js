33e9501ecdb3b6d878d124b73140a76dc1683e5d
Nikolai Cinotti <eaenikolai@gmail.com>
2025-10-10 22:40:20 -0300

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -259,7 +259,7 @@ const about = {
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
     display: true,
-    subItems: false,
+    subItems: true,
   },
   avatar: {
     display: true,
