6d21cd33413fa592ee5c486bc0eaaa47e4d77fb5
Nikolai Cinotti <eaenikolai@gmail.com>
2025-11-11 19:11:15 -0300

    Update content.js

--- src/resources/content.js
+++ src/resources/content.js
@@ -239,7 +239,7 @@ const home = {
   headline: <>Solving real-world problems through gamification</>,
   featured: {
     display: true,
-    title: <>Recent project: <strong className="ml-4">Gendrify ID</strong></>,
+    title: <>Recent project: <strong className="ml-4">LIT. Live. Improved. Together</strong></>,
     href: "/work/lit",
   },
   subline: (
